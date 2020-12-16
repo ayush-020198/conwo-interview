0e02a75f3f38c2524bd34a43ff78b576cf518dbf
ayush-020198 <ayush.shrivastavaujn@gmail.com>
2020-12-16 22:22:57 +0530

    connect backend to frontend

--- client/src/Model/Form.js
+++ client/src/Model/Form.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 import './Form.css'
 class Form extends Component{
     constructor(props){
@@ -27,31 +28,48 @@ class Form extends Component{
         })
     }
 
-    onCreateSchedule=()=>{
-        console.log(this.state.customer)
-    }
+    onCreateSchedule = event => {
+        event.preventDefault();
+        const user = {
+          customer: {
+            sLat: this.state.customer.sLat,
+            sLong: this.state.customer.sLong,
+            dLat: this.state.customer.dLat,
+            dLong: this.state.customer.dLong,
+            time: this.state.customer.time,
+            email: this.state.customer.email
+          }
+        }
+        Axios.post('https://jsonplaceholder.typicode.com/users', { user })
+          .then(res=>{
+            console.log(res);
+            console.log(res.data);
+          })
+      }
 
     render(){
         return (
             <div className="infoBox">
                 <div className="card" style={{width: "25%"}}>
                     <div className="card-body">
-                        <div className="mb-3" style={{textAlign: "left"}}>
-                        <label>Source: Latitude <div style={{textAlign: "right"}}><input type="number" name="sLat" value={this.state.customer.sLat} onChange={this.changeHandler}/></div></label>
-                        <label>Source: Longitude <div style={{textAlign: "right"}}><input type="number" name="sLong" value={this.state.customer.sLong} onChange={this.changeHandler}/></div></label>
-                        </div>                       
-                        <div className="mb-3" style={{textAlign: "left"}}>
-                        <label>Dest: Latitude <div style={{textAlign: "right"}}><input type="number" name="dLat" value={this.state.customer.dLat} onChange={this.changeHandler}/></div></label>
-                        <label>Dest: Longitude <div style={{textAlign: "right"}}><input type="number" name="dLong" value={this.state.customer.dLong} onChange={this.changeHandler}/></div></label>
-                        </div>
-                        <div className="mb-3" style={{textAlign: "left"}}>
-                        <label>Time: <input type="time" name="time" value={this.state.customer.time} onChange={this.changeHandler}/></label> 
-                       </div>
-                        <div className="mb-3">
-                        <div style={{textAlign: "left"}}><label className="form-label">Email: <input type="email" name="email" value={this.state.customer.email} onChange={this.changeHandler}/></label></div>
-                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-                        </div>
-                        <button onClick={this.onCreateSchedule} className="btn btn-primary">Submit</button>
+                        <form onSubmit={this.onCreateSchedule}>
+                            <div className="mb-3" style={{textAlign: "left"}}>
+                            <label>Source: Latitude <div style={{textAlign: "right"}}><input type="number" name="sLat" value={this.state.customer.sLat} onChange={this.changeHandler}/></div></label>
+                            <label>Source: Longitude <div style={{textAlign: "right"}}><input type="number" name="sLong" value={this.state.customer.sLong} onChange={this.changeHandler}/></div></label>
+                            </div>                       
+                            <div className="mb-3" style={{textAlign: "left"}}>
+                            <label>Dest: Latitude <div style={{textAlign: "right"}}><input type="number" name="dLat" value={this.state.customer.dLat} onChange={this.changeHandler}/></div></label>
+                            <label>Dest: Longitude <div style={{textAlign: "right"}}><input type="number" name="dLong" value={this.state.customer.dLong} onChange={this.changeHandler}/></div></label>
+                            </div>
+                            <div className="mb-3" style={{textAlign: "left"}}>
+                            <label>Time: <input type="time" name="time" value={this.state.customer.time} onChange={this.changeHandler}/></label> 
+                            </div>
+                            <div className="mb-3">
+                            <div style={{textAlign: "left"}}><label className="form-label">Email: <input type="email" name="email" value={this.state.customer.email} onChange={this.changeHandler}/></label></div>
+                            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                            </div>
+                            <button type="submit" className="btn btn-primary">Remind Me!</button>
+                        </form>
                     </div>  
                 </div>
           </div>
---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.send('Hello');
+})
+
+app.get('/schedule', (req, res) => {
+    const customer = {
+        sLat: req.body.sLat,
+        sLong: req.body.sLong,
+        dLat: req.body.dLat,
+        dLong: req.body.dLong,
+        time: req.body.time,
+        email: req.body.email
+    }
+
+    res.send(req.body);
+})
+
+app.listen(process.env.PORT || 3001, () => {
+    console.log('Server is running on Port 3001')
+})
